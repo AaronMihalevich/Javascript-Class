6e2a75368ed7ea2f6d08c625b45e12a8800f1817
AaronMihalevich <43824835+AaronMihalevich@users.noreply.github.com>
2019-09-23 20:22:02 -0600

    major changes to exam 1 files

--- Exam 1/scripts.js
+++ Exam 1/scripts.js
@@ -2,13 +2,7 @@
 // Exam 1 : CIS-131
 // 09/19/2019
 
-var mj = 9.99;
-var md = 10.99;
-var rmh = 12.99;
-var dd = 9.99;
-var dl = 11.99;
-var la = 4.20;
-var tax = 1.076;
+
 
 
 
@@ -20,11 +14,12 @@ var pizza = ["The Mary Jane", "The Magic Dragon", "The Rocky Mountain High",
 
 
 
+
 // ************type of pizza options************
 var p = 0;
 for (p = 0; p < pizza.length; p++) {
   document.getElementById('pizzaOptions').innerHTML +=
-    "<option value='0'>" + pizza[p] + "</option>"
+    "<option value=''>" + pizza[p] + "</option>"
 }
 
 
@@ -33,7 +28,7 @@ for (p = 0; p < pizza.length; p++) {
 var i = 0;
 for (i = 1; i < 11; i++) {
   document.getElementById('numOptions').innerHTML +=
-    "<option value='1'>" + i + "</option>"
+    "<option value=''>" + i + "</option>"
 }
 
 
@@ -43,32 +38,98 @@ for (i = 1; i < 11; i++) {
 
 // ***********error messages***********
 
-function vals(form) {
+// function vals(frm) {
 
-  var isValid = true;
-  if (form.name.value == "") {
-    isValid = false;
-    document.getElementById('nameErr').innerHTML = "<h3 class='error'>" + "Yo, man! You gotta put your name in man!" + "</h3>"
-    form.name.style.backgroundColor = "orange";
+//   var isValid = true;
+//   if (frm.name.value == "") {
+//     isValid = false;
+//     document.getElementById('nameErr').innerHTML = "<h3 class='error'>" + "Yo, man! You gotta put your name in man!" + "</h3>"
+//     frm.name.style.backgroundColor = "orange";
 
-  }
+//   }
 
-  if (form.phone.value == "") {
-    isValid = false;
-    document.getElementById('phoneErr').innerHTML = "<h3 class='error'>" + "Man, that's not a real number man.... is it?" + "</h3>"
-    form.phone.style.backgroundColor = "orange";
-  }
+//   if (frm.phone.value == "") {
+//     isValid = false;
+//     document.getElementById('phoneErr').innerHTML = "<h3 class='error'>" + "Man, that's not a real number man.... is it?" + "</h3>"
+//     frm.phone.style.backgroundColor = "orange";
+//   }
 
-  return isValid;
+//   return isValid;
+// }
+
+
+//******************Output section**********************
+
+document.getElementById('myclick').addEventListener('click', order())
+
+function order()
+{
+  var theOrder = document.getElementById('myForm');
+  var code = "";
+
+  console.log(theOrder.elements)
+
+  // document.getElementById('order').innerHTML = 
+  //   "<h2>Hey man, thank you for your order!</h2> <p class='output' id='orderName'></p>" +
+  //   "<h2>Yo man, we'll give you a call at:</h2> <p class='output' id='orderNumber'></p>" +
+  //   "<h2>Hey, great choice man! You ordered the:</h2> <p class='output' id='pizzaType'></p>" +
+  //   "<h2>Hey man, we'll start BAKING " + numValue[1] + " pizza(s)</h2>" +
+  //   "<h2 class='total'>Man, the total on that is going to be:</h2> <h4 id='subTotal'></h4>"
+
+
+
 }
 
-/////////////////////////////////////////////////////////
-// ******************Output section**********************
-/////////////////////////////////////////////////////////
-// document.getElementById('myclick').addEventListener('click', myFunc())
+
+
+// **************order output*****************
+// document.getElementById('myclick').addEventListener('click', function()
 // {
+//   var query = window.location.search.substring(1);
+//   var pairs = query.split("&");
+//   var nameValue = pairs[0].split("=");
+//   var phoneValue = pairs[1].split("=");
+//   var pizzaValue = pairs[2].split("=");
+//   var numValue = pairs[3].split("=");
+//   var pizzaName = pizzaValue[1];
+  
+//   console.log(phoneValue[1])
+// // ****************order details***************
+//   document.getElementById('order').innerHTML = 
+//     "<h2>Hey man, thank you for your order!</h2> <p class='output' id='orderName'></p>" +
+//     "<h2>Yo man, we'll give you a call at:</h2> <p class='output' id='orderNumber'></p>" +
+//     "<h2>Hey, great choice man! You ordered the:</h2> <p class='output' id='pizzaType'></p>" +
+//     "<h2>Hey man, we'll start BAKING " + numValue[1] + " pizza(s)</h2>" +
+//     "<h2 class='total'>Man, the total on that is going to be:</h2> <h4 id='subTotal'></h4>"
 
-}
-console.log(pizza);
-console.log(i);
+//   document.getElementById('orderName').innerHTML = nameValue[1];
+//   document.getElementById('orderNumber').innerHTML = phoneValue[1];
+//   document.getElementById('pizzaType').innerHTML = pizza[pizzaName]; 
+
+  
+
+//   // *****************order subtotal*****************
+
+//   document.getElementById('subTotal').innerHTML = "$" + 10 * (numValue[1]);
+
+// });
+
+
+
+
+
+
+// function printQuery()
+// {
+//     var query = window.location.search.substring(1);
+//     var valPairs = query.split("&");
+//     for (var i = 0; i < valPairs.length; i++)
+//     {
+//         var pair = valPairs[i].split("=")
+//         document.getElementById('order').innerHTML += (`${pair[0]} gets ${pair[1]}<br>` )
+//     }
+// }
+
+
+console.log(pizza)
 console.log(p);
