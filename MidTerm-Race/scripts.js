cff2be2a0ab7e356fd837e8747faa5b2afaabc3b
AaronMihalevich <43824835+AaronMihalevich@users.noreply.github.com>
2019-09-24 20:08:53 -0600

    upload midterm race

---
+++ MidTerm-Race/scripts.js
@@ -0,0 +1,13 @@
+// Aaron Mihalevich
+// Mid Term Race Game
+// CIS-131 09/24/2019
+
+document.getElementById('redLight').addEventListener('click', function()
+    {
+        document.getElementById('redLight').src = 'red-light.png'
+    });
+
+    document.getElementById('greenLight').addEventListener('click', function()
+    {
+        document.getElementById('greenLight').src = 'green-light.png'
+    });
