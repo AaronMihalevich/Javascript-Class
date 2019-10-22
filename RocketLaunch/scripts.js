3fa1164f7d0d8a790163b8573bac70eb9ce6a8e9
Aaron M Mac <aaronmihalevich@Aarons-iMac.attlocal.net>
2019-10-22 15:22:05 -0500

    completion of rocket assignment

--- RocketLaunch/scripts.js
+++ RocketLaunch/scripts.js
@@ -7,88 +7,188 @@
 //LIST: LAUNCH DATE AND TIME(.net), LAUNCH NAME(.name)
 //LAUNCH TYPE: (.lsp) FALCON = 121, LAUNCHERONE = 199, ARIANE = 115
 
-//**************** GETS DATA FROM SITE */
-//**************** GETS DATA FROM SITE */
-//**************** GETS DATA FROM SITE */
 
+//**************** NEXT 5 LAUNCH FUNCTION (START) */
+window.addEventListener('load', pageLoad)
 
-var httpRequest = new XMLHttpRequest();
-httpRequest.open("get","https://launchlibrary.net/1.4/launch?next=107");
-httpRequest.send(null);
-httpRequest.onreadystatechange = launchFunction;
-
-
-function launchFunction()
+function pageLoad()
 {
-    if(httpRequest.readyState == 4 && httpRequest.status == 200)
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open("get","https://launchlibrary.net/1.4/launch?next=5");
+    httpRequest.send(null);
+    httpRequest.onreadystatechange = loadFunction;
+
+
+    function loadFunction()
     {
-        //**************** PAGE LOAD AFTER GETTING DATA */
-        //GRAB JSON STRING
-        var request = httpRequest.responseText;
+        if(httpRequest.readyState == 4 && httpRequest.status == 200)
+        {
+            //**************** PAGE LOAD AFTER GETTING DATA */
+            //GRAB JSON STRING
+            var request = httpRequest.responseText;
 
-        //CONVERT TO JS OBJECT
-        var launchObject = JSON.parse(request);
-        var launch = launchObject.launches;
-
-        console.log(launch)
+            //CONVERT TO JS OBJECT
+            var launchObject = JSON.parse(request);
+            var launch = launchObject.launches;
         
-       // SET VARIABLES FOR HEAD AND LIST IDS
-       var headDate = document.getElementById('date');
-       var ul = document.getElementById('list');
+        
+        // SET VARIABLES FOR HEAD AND LIST IDS
+        
+            var headDate = document.getElementById('date');
+            var l1 = document.getElementById('launch1');
+            var l2 = document.getElementById('launch2');
+            var l3 = document.getElementById('launch3');
+            var l4 = document.getElementById('launch4');
+            var l5 = document.getElementById('launch5');
 
-       
-       headDate.innerHTML = "<strong>Next launch: </strong>" + launchObject.launches[0].net
+            headDate.innerHTML = "<strong>Next launch: </strong>" + launch[0].net;
+            l1.innerHTML = launch[0].net + " -- " + launch[0].name;
+            l2.innerHTML = launch[1].net + " -- " + launch[1].name;
+            l3.innerHTML = launch[2].net + " -- " + launch[2].name;
+            l4.innerHTML = launch[3].net + " -- " + launch[3].name;
+            l5.innerHTML = launch[4].net + " -- " + launch[4].name;
 
-        for (var i=0; i < 5; i++)
-        {
-            // PUT FIRST 5 LAUNCHES INTO HEAD AND LIST   
-            ul.innerHTML += `<li>${(i+1) + " - " + launchObject.launches[i].net + " " + launchObject.launches[i].name}</li>`
-     
-        }
+            // SET COUNT DOWN TIMER
+            var countDate = new Date(launch[0].windowstart).getTime();
 
-       
-        //**************** LAUNCH TYPE SECTION */
-        var falconLaunch = [];
-        var launcheroneLaunch = [];
-        var arianeLaunch = [];
-
-        for (var p=0; p < launch.length; p++)
-        {
-            // var launchID = launch[p];
-            // 
-            // 
-            // 
-
-            if (launch[p].lsp == 121)
+            var timer = setInterval(function()
             {
-                falconLaunch.push(launch[p]);
-                console.log(launch[p])
-            }
+                var time = new Date().getTime();
+                var diff = countDate - time;
 
-            if (launch[p].lsp == 199)
-            {
-                launcheroneLaunch.push(launch[p]);
-                console.log(launch[p])
-            }
+                var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var min = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+                var sec = Math.floor((diff % (1000 * 60)) / 1000);
 
-            if (launch[p].lsp == 115)
-            {
-                arianeLaunch.push(launch[p]);
-                console.log(launch[p])
-            }
-
-            
-
+                document.getElementById('timer').innerHTML = days + "d " + hours + "h " + min + "m " + sec + "s ";
+            });
 
         }
-        // console.log(falconLaunch[0])
-        // console.log(falconLaunch[1])
-        // console.log(falconLaunch[2])
-       
-
-    
-
-
-
     }
 }
+//**************** NEXT 5 LAUNCH FUNCTION (END) */
+
+
+//**************** FALCON LAUNCH FUNCTION (START) */
+function falconLaunch()
+{
+    var falconRequest = new XMLHttpRequest();
+    falconRequest.open("get","https://launchlibrary.net/1.4/launch?name=falcon&next=5");
+    falconRequest.send(null);
+    falconRequest.onreadystatechange = falconFunction;
+
+    function falconFunction()
+    {
+        if(falconRequest.readyState == 4 && falconRequest.status == 200)
+        {
+            //**************** PAGE LOAD AFTER GETTING DATA */
+            //GRAB JSON STRING
+            var fRequest = falconRequest.responseText;
+
+            //CONVERT TO JS OBJECT
+            var falconObject = JSON.parse(fRequest);
+            var falcon = falconObject.launches;
+
+        
+        
+            // SET VARIABLES FOR HEAD AND LIST IDS
+            var l1 = document.getElementById('launch1');
+            var l2 = document.getElementById('launch2');
+            var l3 = document.getElementById('launch3');
+            var l4 = document.getElementById('launch4');
+            var l5 = document.getElementById('launch5');
+
+            l1.innerHTML = falcon[0].net + " -- " + falcon[0].name;
+            l2.innerHTML = falcon[1].net + " -- " + falcon[1].name;
+            l3.innerHTML = falcon[2].net + " -- " + falcon[2].name;
+            l4.innerHTML = falcon[3].net + " -- " + falcon[3].name;
+            l5.innerHTML = falcon[4].net + " -- " + falcon[4].name;
+       
+        }
+    }
+}
+//**************** FALCON LAUNCH FUNCTION (END) */
+
+
+
+
+//**************** LAUNCHERONE FUNCTION (START) */
+function launcheroneLaunch()
+{
+    var launcherRequest = new XMLHttpRequest();
+    launcherRequest.open("get","https://launchlibrary.net/1.4/launch?name=launcherone&next=5");
+    launcherRequest.send(null);
+    launcherRequest.onreadystatechange = launcherFunction;
+
+    function launcherFunction()
+    {
+        if(launcherRequest.readyState == 4 && launcherRequest.status == 200)
+        {
+            //**************** PAGE LOAD AFTER GETTING DATA */
+            //GRAB JSON STRING
+            var lRequest = launcherRequest.responseText;
+
+            //CONVERT TO JS OBJECT
+            var launcherObject = JSON.parse(lRequest);
+            var launcher = launcherObject.launches;
+        
+        
+            // SET VARIABLES FOR HEAD AND LIST IDS
+            var l1 = document.getElementById('launch1');
+            var l2 = document.getElementById('launch2');
+            var l3 = document.getElementById('launch3');
+            var l4 = document.getElementById('launch4');
+            var l5 = document.getElementById('launch5');
+
+            l1.innerHTML = launcher[0].net + " -- " + launcher[0].name;
+            l2.innerHTML = launcher[1].net + " -- " + launcher[1].name;
+            l3.innerHTML = launcher[2].net + " -- " + launcher[2].name;
+            l4.innerHTML = launcher[3].net + " -- " + launcher[3].name;
+            l5.innerHTML = launcher[4].net + " -- " + launcher[4].name;
+        }
+    }
+}
+//**************** LAUNCHERONE LAUNCH FUNCTION (END) */
+
+
+
+
+//**************** ARIANE FUNCTION (START) */
+function arianeLaunch()
+{
+    var arianeRequest = new XMLHttpRequest();
+    arianeRequest.open("get","https://launchlibrary.net/1.4/launch?name=ariane&next=5");
+    arianeRequest.send(null);
+    arianeRequest.onreadystatechange = arianeFunction;
+
+    function arianeFunction()
+    {
+        if(arianeRequest.readyState == 4 && arianeRequest.status == 200)
+        {
+            //**************** PAGE LOAD AFTER GETTING DATA */
+            //GRAB JSON STRING
+            var aRequest = arianeRequest.responseText;
+
+            //CONVERT TO JS OBJECT
+            var arianeObject = JSON.parse(aRequest);
+            var ariane = arianeObject.launches;
+        
+        
+            // SET VARIABLES FOR HEAD AND LIST IDS
+            var l1 = document.getElementById('launch1');
+            var l2 = document.getElementById('launch2');
+            var l3 = document.getElementById('launch3');
+            var l4 = document.getElementById('launch4');
+            var l5 = document.getElementById('launch5');
+
+            l1.innerHTML = ariane[0].net + " -- " + ariane[0].name;
+            l2.innerHTML = ariane[1].net + " -- " + ariane[1].name;
+            l3.innerHTML = ariane[2].net + " -- " + ariane[2].name;
+            l4.innerHTML = ariane[3].net + " -- " + ariane[3].name;
+            l5.innerHTML = ariane[4].net + " -- " + ariane[4].name;
+        }
+    }
+}
+//**************** ARIANE LAUNCH FUNCTION (END) */
+       
