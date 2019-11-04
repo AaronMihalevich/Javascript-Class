399882a1b634392e06b68fc9b6d4e125165f462b
Aaron M Mac <aaronmihalevich@Aarons-iMac.attlocal.net>
2019-11-04 11:24:28 -0600

    new

---
+++ Assign_VueComp/scripts.js
@@ -0,0 +1,59 @@
+// Create a page that:
+
+//     Utilizes an ajax call (either as a jQuery method, or as a XMLHttpRequest Object) to the movie database (Use Movies – Now Playing)
+//     Load the movies into an array (or an object) outside of the Vue instance
+//     Link your array to the Vue instance and then utilize the Vue instance in your html
+//     Utilizes a VUE component. A good candidate for a component here is a Bootstrap Card component
+//     Each component must show the Image, Title and brief description
+//     Utilizing the Vue component you created, display three cards displaying the top three movies.
+
+// Note: for this assignment you do not have to use child components as we did in class. But you must create at least one component which will be the template for your card displays
+
+var httpRequest = new XMLHttpRequest();
+        httpRequest.open("get", "https://api.themoviedb.org/3/movie/now_playing?api_key=20c38770f322031e9daaec50e14cfe4e&language=en-US&page=1");
+        httpRequest.send(null);
+        httpRequest.onreadystatechange = aFunction;
+
+        function aFunction()
+        {
+            if (httpRequest.readyState == 4 && httpRequest.status == 200)
+            {
+                var moviesRequest = httpRequest.responseText;
+                var jsObject = JSON.parse(moviesRequest);
+                var topMovies = jsObject.results;
+                console.log(jsObject.results)
+                console.log(topMovies)
+                
+
+                Vue.component('movie-list',{
+                    props:['themovies'],
+                    template:  
+                    `
+                        <div>
+                            <h2><slot></slot></h2>
+                            <div v-for="movie in themovies">
+                                <p>{{movie.title}}</p>
+                            </div>
+                        </div>
+
+
+
+                        
+                        <div class="card" style="width: 18rem;" v-for="movie in themovies">
+                            <div class="card-body">
+                                <h5 class="card-title">{{movie.title}}</h5>
+                                <p class="card-text">{{movie.overview}}</p>
+                            </div>
+                        </div>
+                    `,
+                })
+
+
+                var app = new Vue ({
+                    el: "#app",
+                    data: {
+                        movies: topMovies,
+                    }
+                })
+            }
+        }
